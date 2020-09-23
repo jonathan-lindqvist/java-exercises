cd97b99c74992ff64ee5348bdb9cc11adf6a0c3e
Jonathan Lindqvist <jonathan.x.lindqvist@gmail.com>
2020-09-23 16:48:19 +0200

    Start on exercise6 week4

--- week4/week4/src/exercises/Ex6SJT.java
+++ week4/week4/src/exercises/Ex6SJT.java
@@ -21,9 +21,9 @@ public class Ex6SJT {
 
     private void program() {
         // Call permutation method
-        List<String> perms = null;  // Replace null with permutationSJT("123456");
+        List<String> perms = permutationSJT("123456");
 
-        out.println(perms.size());  // 720
+        out.println(perms.size() == 720);  // 720
         for (String s : perms) {
             out.println(s);
         }
@@ -40,4 +40,52 @@ public class Ex6SJT {
     // Steinhaus–Johnson–Trotter permutation algorithm iterative!
     // Use functional decomposition!
     // TODO
+    List<String> permutationSJT(String s) {
+        List<String> permutations = new ArrayList<>();
+        String[] arr = s.split("");
+        // false is left, true is right.
+        boolean[] dir = new boolean[arr.length];
+
+        // Add first permutation.
+        permutations.add(s);
+        while(gotMobiles(arr,dir)){
+            // Find greatest mobile value and move
+            // move until it can not move
+            // find next greatest mobile value
+            // then invert dir for all values that are greater than the current largest mobile value
+
+            int indexLargestMobile = getIndexOfLargestMobile(arr, dir);
+        }
+
+        return permutations;
+    }
+    boolean gotMobiles(String[] arr, boolean[] dir){
+        boolean anyMobile = false;
+
+        /*
+        To be mobile it needs to be greater than the value it is pointing to.
+         */
+
+        for(int i = 0; i < arr.length; i++){
+            boolean dirForIndex = dir[i];
+            if(i-1 >= 0 && i+1 < arr.length){
+                int neighbourIndex = (dirForIndex) ? i+1 : i-1;
+                String neighbourValue = arr[neighbourIndex];
+                if(Integer.parseInt(arr[i]) > Integer.parseInt(neighbourValue)){
+                    anyMobile = true;
+                    break;
+                }
+            }
+
+        }
+
+        return anyMobile;
+    }
+
+    int getIndexOfLargestMobile(String[] arr, boolean[] dir){
+        for(int i = 0; i < arr.length;i++){
+            
+        }
+        return 0;
+    }
 }
