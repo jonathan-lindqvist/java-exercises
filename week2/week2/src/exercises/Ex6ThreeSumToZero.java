8dc696d2967e78dd4f8b71292669fad98742ff9f
Jonathan Lindqvist <jonathan.x.lindqvist@gmail.com>
2020-10-09 11:54:22 +0200

    WOrking code for exercise6 week2

--- week2/week2/src/exercises/Ex6ThreeSumToZero.java
+++ week2/week2/src/exercises/Ex6ThreeSumToZero.java
@@ -29,58 +29,27 @@ public class Ex6ThreeSumToZero {
     }
 
     int[] getThreeSum(int[] arr) {
+        int[] threeIndices = new int[3];
+        int leftIndex,rightIndex;
 
-        // fix code later, this works
-        boolean found = false;
-
-        // sort array elements
-        Arrays.sort(arr);
-
-        for (int i=0; i<n-1; i++)
-        {
-            // initialize left and right
-            int l = i + 1;
-            int r = n - 1;
-            int x = arr[i];
-            while (l < r)
-            {
-                if (x + arr[l] + arr[r] == 0)
-                {
-                    // print elements if it's sum is zero
-                    System.out.print(x + " ");
-                    System.out.print(arr[l]+ " ");
-                    System.out.println(arr[r]+ " ");
-
-                    l++;
-                    r--;
-                    found = true;
+        for(int i = 0; i < arr.length-2; i++){
+            leftIndex = i+1;
+            rightIndex = arr.length-1;
+            while(leftIndex<rightIndex){
+                if(arr[i] + arr[leftIndex] + arr[rightIndex] == 0){
+                    threeIndices[0] = i;
+                    threeIndices[1] = leftIndex;
+                    threeIndices[2] = rightIndex;
+                    return threeIndices;
+                }else if(arr[i] + arr[leftIndex] + arr[rightIndex] < 0){
+                    leftIndex++;
+                }else{
+                    rightIndex--;
                 }
-
-                // If sum of three elements is less
-                // than zero then increment in left
-                else if (x + arr[l] + arr[r] < 0)
-                    l++;
-
-                    // if sum is greater than zero than
-                    // decrement in right side
-                else
-                    r--;
             }
         }
-
-        if (found == false)
-            System.out.println(" No Triplet Found");
+        int[] test = new int[0];
+        return test;
     }
-
-    // Driven source
-    public static void main (String[] args) {
-
-        int arr[] = {0, -1, 2, -3, 1};
-        int n =arr.length;
-        findTriplets(arr, n);
-    }
-//This code is contributed by Tushil..
-    }
-
 
 }
